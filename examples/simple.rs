8989c52cc3386996e06f414a24b91d9a3e4d6de4
mintlu8 <mintlux667@gmail.com>
2025-03-10 22:50:57 +0800

    Made simple a 2d test.

--- examples/simple.rs
+++ examples/simple.rs
@@ -4,12 +4,14 @@ use bevy::{
     app::{App, Startup},
     asset::{AssetServer, Assets},
     color::{Color, Srgba},
+    core_pipeline::core_2d::Camera2d,
     math::{Vec2, Vec3},
-    pbr::{AmbientLight, MeshMaterial3d, StandardMaterial},
+    pbr::AmbientLight,
     prelude::{
-        AlphaMode, Camera3d, Commands, Mesh, Mesh3d, OrthographicProjection, Plane3d, Projection,
-        Res, ResMut, Transform,
+        Commands, Mesh, OrthographicProjection, Plane3d, Projection, Res, ResMut, Transform,
     },
+    render::mesh::Mesh2d,
+    sprite::{AlphaMode2d, ColorMaterial, MeshMaterial2d},
     DefaultPlugins,
 };
 use bevy_rich_text3d::{Text3d, Text3dPlugin, Text3dStyling, TextAtlas};
@@ -25,12 +27,11 @@ pub fn main() {
             color: Color::WHITE,
             brightness: 800.,
         })
-        .add_systems(Startup, |mut commands: Commands, server: Res<AssetServer>, mut standard_materials: ResMut<Assets<StandardMaterial>>| {
+        .add_systems(Startup, |mut commands: Commands, server: Res<AssetServer>, mut standard_materials: ResMut<Assets<ColorMaterial>>| {
             let mat = standard_materials.add(
-                StandardMaterial {
-                    base_color_texture: Some(TextAtlas::DEFAULT_IMAGE.clone_weak()),
-                    alpha_mode: AlphaMode::Blend,
-                    unlit: true,
+                ColorMaterial {
+                    texture: Some(TextAtlas::DEFAULT_IMAGE.clone_weak()),
+                    alpha_mode: AlphaMode2d::Blend,
                     ..Default::default()
                 }
             );
@@ -43,8 +44,8 @@ pub fn main() {
                     stroke_color: Srgba::BLACK,
                     ..Default::default()
                 },
-                Mesh3d::default(),
-                MeshMaterial3d(mat.clone()),
+                Mesh2d::default(),
+                MeshMaterial2d(mat.clone()),
             ));
 
             commands.spawn((
@@ -53,18 +54,18 @@ pub fn main() {
                     color: Srgba::new(0., 1., 1., 1.),
                     ..Default::default()
                 },
-                Mesh3d::default(),
-                MeshMaterial3d(mat.clone()),
+                Mesh2d::default(),
+                MeshMaterial2d(mat.clone()),
                 Transform::from_translation(Vec3::new(50., -100., 0.))
             ));
 
             commands.spawn((
-                Mesh3d(server.add(Mesh::from(Plane3d::new(Vec3::Z, Vec2::new(200., 200.))))),
-                MeshMaterial3d(mat.clone()),
+                Mesh2d(server.add(Mesh::from(Plane3d::new(Vec3::Z, Vec2::new(200., 200.))))),
+                MeshMaterial2d(mat.clone()),
                 Transform::from_translation(Vec3::new(0., 100., -1.))
             ));
             commands.spawn((
-                Camera3d::default(),
+                Camera2d,
                 Projection::Orthographic(OrthographicProjection::default_3d()),
                 Transform::from_translation(Vec3::new(0., 0., 1.))
                     .looking_at(Vec3::new(0., 0., 0.), Vec3::Y)
